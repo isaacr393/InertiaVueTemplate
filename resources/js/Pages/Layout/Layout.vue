7712a6929643d107e260404fece493818b688332
Isaac Rivas <isaacrivas140@gmail.com>
2023-02-25 19:57:07 -0400

    Version inicial con sidebar y top header

---
+++ resources/js/Pages/Layout/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+    <body class="fix-header fix-sidebar card-no-border">
+        <div id="main-wrapper">
+            <Header />
+            <Sidebar />
+        </div>       
+    </body>
+</template>
+
+<script>
+import Header from './Header.vue';
+import Sidebar from './Sidebar.vue';
+
+export default {
+    components : { Sidebar,Header  }
+}
+</script>
+
+<style>
+
+</style>
